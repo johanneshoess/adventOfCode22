87ea90968a9d66b20b9e1b88ab635ccd67f7bc4c
Johannes Höß <johannes@hoess-ottersweier.de>
2022-12-02 14:03:50 +0100

    python lössung

---
+++ python/01.py
@@ -0,0 +1,15 @@
+f = open("../input/01.txt", "r")
+lines = f.readlines()
+
+elfes = []
+count = 0
+for line in lines:
+    if line == "\n":
+        elfes.append(count)
+        count = 0
+    else:
+        line = int(line)
+        count += line
+
+elfes.sort()
+print(elfes[-1])
